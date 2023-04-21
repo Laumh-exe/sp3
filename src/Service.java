08960b2c80c47f5dd3df4a99eb2317b278fc8c7a
Laumh-exe <lauritzhauge@gmail.com>
2023-04-21 11:48:31 +0200

    Added Service class with methods

---
+++ src/Service.java
@@ -0,0 +1,47 @@
+import java.util.LinkedList;
+import java.util.List;
+
+public class Service {
+    private User currenUser;
+    private List<User> users;
+    private List<AMedia> media;
+    public Service() {
+
+    }
+
+    private void dataSetup() {
+
+    }
+
+    private void userSetup() {
+
+    }
+
+    private void mainMenu() {
+
+    }
+
+    private void showMovies() {
+
+    }
+
+    private void showSeries() {
+
+    }
+
+    private boolean login(String username, String password) {
+        return false;
+    }
+
+    private list<AMedia> searchMedia(String title) {
+        //return list
+    }
+
+    private list<AMedia> searchByGenre(Genre genre) {
+        //return list
+    }
+
+    private void onClose() {
+
+    }
+}
