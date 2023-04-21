b647d8f346475515c7ee6b191f0f7b4085b23e01
TRossen89 <110735176+TRossen89@users.noreply.github.com>
2023-04-21 12:23:24 +0200

    dsf

--- src/Service.java
+++ src/Service.java
@@ -5,42 +5,101 @@ public class Service {
     private User currenUser;
     private List<User> users;
     private List<AMedia> media;
+
+
+
+
+    // Lauritz
     public Service() {
 
+
     }
 
+
+
+    // Tobias
     private void dataSetup() {
 
+
     }
 
+
+
+
+    // Lauritz
     private void userSetup() {
 
+        // Asks: Login og Create user
+        // Execute methods accordingly: Login or Create user
+
     }
 
+
+
+    // Lauritz
     private void mainMenu() {
 
+        /* kalder:
+
+        dataSetup()
+        userSetup()
+            viser:
+            Hovedmenu (De 4 valgmuligheder brugeren har når han/hun/hen er logget ind eller har oprettet
+            en bruger)
+
+            1) Søg efter medie
+            2) Søg efter alle medier i en kategori
+            3) Se sete medier
+            4) Se gemte medier
+
+         */
+
+
     }
+
+
+
+
+    //Lauritz by choice
 
     private void showMovies() {
 
     }
 
+    //Lauritz by choice
     private void showSeries() {
 
     }
+
+
+
+
+
+    // Tobias
 
     private boolean login(String username, String password) {
         return false;
     }
 
+
+
+    // Lauritz
+
     private list<AMedia> searchMedia(String title) {
         //return list
+
+
     }
 
+    //Lauritz
     private list<AMedia> searchByGenre(Genre genre) {
         //return list
     }
 
+
+
+
+    // Tobias
     private void onClose() {
 
     }
