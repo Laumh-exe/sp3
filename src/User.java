14632b080ff92b4e0a26d8daf661be6197f40cba
DoctorHeyde <alexanderrasmuss@gmail.com>
2023-04-24 11:01:18 +0200

    All method names, for User class
    All methods for user class

--- src/User.java
+++ src/User.java
@@ -1,2 +1,22 @@
+import java.util.Set;
+
 public class User {
+
+    private String userName;
+    private String password;
+    private Set<AMedia> watchedMedia;
+    private Set<AMedia> watchlist;
+
+    public String getName(){
+        return userName;
+    }
+    public boolean comparePassword(String password){
+        return true;
+    }
+    public void addToWatchList(AMedia media){
+        
+    }
+    public Set<AMedia> getWatchList(){
+        return
+    }
 }
