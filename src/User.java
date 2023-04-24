8066d3efad9e04164638cd3a37a7e680a6b8a056
cph-dh223 <124783509+cph-dh223@users.noreply.github.com>
2023-04-24 13:16:40 +0200

    User (#2)
    * Update classDiagra.puml
    
    * Create genre enum
    
    * added all media classes
    
    * Update .gitignore
    
    * Update classDiagra.puml
    
    * Create AMedieaTests.java
    
    * User class
    
    Adding User class to see if my github is a robot that is listening to me.
    
    * testing getGenre on AMedia
    
    * updated to save what type of media AMedia contains
    
    * Update classDiagra.puml
    
    * All method names, for User class
    
    All methods for user class
    
    * Update User.java
    
    * New Hashset
    
    New set
    
    * Update User.java
    
    added a cunstroctor and wathedMedia add and get
    
    * comparePassword
    
    If password is equal to this.password then comparePassword needs to return true.
    
    ---------
    
    Co-authored-by: DoctorHeyde <alexanderrasmuss@gmail.com>

---
+++ src/User.java
@@ -0,0 +1,51 @@
+import java.util.HashSet;
+import java.util.Set;
+
+import media.AMedia;
+
+public class User {
+
+    private String userName;
+    private String password;
+    private Set<AMedia> watchedMedia;
+    private Set<AMedia> watchList;
+
+    public User(String userName, String password){
+        this.userName = userName;
+        this.password = password;
+        watchedMedia = new HashSet<>();
+        watchList = new HashSet<>();
+    }
+
+    public String getName(){
+        return userName;
+    }
+
+    public String getPassword(){
+        return password;
+    }
+    //Todo: Possible password validator?
+    public boolean comparePassword(String password){
+        if(password.equals(this.password)) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    public void addToWatchedMedia(AMedia media){
+        watchedMedia.add(media);
+    }
+
+    public Set<AMedia> getWatchedMedia(){
+        return watchedMedia;
+    }
+
+    public void addToWatchList(AMedia media){
+        watchList.add(media);
+    }
+    
+    public Set<AMedia> getWatchList(){
+        return watchList;
+    }
+}
