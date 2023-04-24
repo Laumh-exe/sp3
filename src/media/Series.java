c52930fc6b4c0a5825355313eec1dd83d6369b2f
cph-dh223 <124783509+cph-dh223@users.noreply.github.com>
2023-04-24 13:14:17 +0200

    A media (#1)
    * Update classDiagra.puml
    
    * Create genre enum
    
    * added all media classes
    
    * Update .gitignore
    
    * Update classDiagra.puml
    
    * Create AMedieaTests.java
    
    * testing getGenre on AMedia
    
    * updated to save what type of media AMedia contains
    
    * Update classDiagra.puml
    
    * finished genres
    
    * Update AMedia.java
    
    rating is now a desmal number
    
    * Update Genre.java
    
    * Revert "Update AMedia.java"
    
    This reverts commit 29213fd4f47a0fdf41aec5fe095a8ec2dd9df513.
    
    * Revert "Revert "Update AMedia.java""
    
    This reverts commit 1060410c39e80877faa0e728ce2e6359c60599fc.
    
    * Revert "Update Genre.java"
    
    This reverts commit 39916f9e75528430f20f81a7bd6df31ebb0527f4.

---
+++ src/media/Series.java
@@ -0,0 +1,42 @@
+package media;
+
+import java.util.List;
+
+public class Series extends AMedia{
+    
+    protected int numSeasons;
+	protected List<Integer> numEpisodes;
+    private int startDate;
+    private int endDate;
+
+    public Series(String title, List<Genre> genres, int rating, String typeOfMedia, int numSeasons, List<Integer> numEpisodes, int startDate, int endDate) {
+        super(title, genres, rating, typeOfMedia);
+        this.numSeasons  = numSeasons;
+        this.numEpisodes = numEpisodes;
+        this.startDate   = startDate;
+        this.endDate     = endDate;
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder sb = new StringBuilder();
+        sb.append(super.toString());
+
+        sb.append("Number of seasons: " + numSeasons + "\n");
+        for (int i = 0; i < numEpisodes.size(); i++) {
+            sb.append("Season ");
+            sb.append(i+1);
+            sb.append(": Number of episodes: ");
+            sb.append(numEpisodes.get(i));
+            sb.append("\n");
+        }
+
+        return sb.toString();
+    }
+
+    @Override
+    protected String getDate() {
+        return "Start date: " + startDate + " end date: " + endDate;
+    }
+    
+}
