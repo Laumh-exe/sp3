37e0f5d71f0d933fcddd3e3d92620e67465749ff
DavidAH <cph-dh223@cphbusiness.dk>
2023-04-25 10:52:46 +0200

    inprot list

--- src/IO.java
+++ src/IO.java
@@ -3,6 +3,7 @@ import java.io.FileNotFoundException;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 import media.AMedia;
 
