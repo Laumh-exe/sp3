d7a629e83c19088ce3c2327d9e450409ddc2683b
DoctorHeyde <alexanderrasmuss@gmail.com>
2023-04-27 13:43:51 +0200

    Last updates

--- src/Service.java
+++ src/Service.java
@@ -13,26 +13,45 @@ public class Service {
 
     private IO io = new IO();
 
-
-    private AMedia getReleaseDate(int releaseDate) {
-        for (AMedia m : media) {
-            if (m.getReleaseDate.equalsIgnoreCase(releaseDate)) {
-                return m.getReleaseDate();
-            }
-        }
-        return null;
-    }
-
     private Collection<AMedia> searchByReleaseDate() {
-        Date date = getReleaseDate();
+         int date = releaseDateInput();
         Collection<AMedia> searchResult = new HashSet<AMedia>();
-        for (AMedia m : media) {
-            for (Date d : m.getReleaseDate()) {
-                if (date == d){
-                    searchResult.add(m);
-                }
-            }
+            for (AMedia m : media) {
+                String listOfMedias = " ";
+               if(m.getReleaseDate()<date){
+                   listOfMedias += m.getTitle() + "\n";
+                   ui.displayMessage(listOfMedias);
+               }
+                /*for (int date d: m.getReleaseDate()) {
+                if (date == d) {
+                    searchResult.add(m);*/
         }
         return searchResult;
+    }
+
+    private int releaseDateInput() {
+        String input = ui.getInput("What year do you want to search for?" +
+                "1) before 1960s\n" +
+                "2) after 1960s and before 2000s\n" +
+                "3) after 2000s\n");
+
+        boolean releaseDate;
+        switch (input) {
+            case "1":
+                releaseDate = 1930 < 1960;
+                break;
+            case "2":
+                releaseDate = 1960 < 1980;
+                break;
+            case "3":
+                releaseDate = 1980 < 2020;
+                break;
+            default:
+                ui.displayMessage("Please type a number to search for release date");
+                releaseDate = null;
+                releaseDateInput();
+        }
+        return releaseDate;
+    }
     }
 }
